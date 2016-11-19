16732143bae4dba3bded7d6e625429cc697f158c
Ka Nguyen <khoaanh2212@gmail.com>
2016-11-19 10:10:53 +0700

    init

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shopping.local/admin/');
+define('HTTP_CATALOG', 'http://shopping.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shopping.local/admin/');
+define('HTTPS_CATALOG', 'http://shopping.local/');
+
+// DIR
+define('DIR_APPLICATION', '/Volumes/DATA/workspace/shopping/admin/');
+define('DIR_SYSTEM', '/Volumes/DATA/workspace/shopping/system/');
+define('DIR_IMAGE', '/Volumes/DATA/workspace/shopping/image/');
+define('DIR_LANGUAGE', '/Volumes/DATA/workspace/shopping/admin/language/');
+define('DIR_TEMPLATE', '/Volumes/DATA/workspace/shopping/admin/view/template/');
+define('DIR_CONFIG', '/Volumes/DATA/workspace/shopping/system/config/');
+define('DIR_CACHE', '/Volumes/DATA/workspace/shopping/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Volumes/DATA/workspace/shopping/system/storage/download/');
+define('DIR_LOGS', '/Volumes/DATA/workspace/shopping/system/storage/logs/');
+define('DIR_MODIFICATION', '/Volumes/DATA/workspace/shopping/system/storage/modification/');
+define('DIR_UPLOAD', '/Volumes/DATA/workspace/shopping/system/storage/upload/');
+define('DIR_CATALOG', '/Volumes/DATA/workspace/shopping/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shopping');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shopping.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shopping.local/');
+
+// DIR
+define('DIR_APPLICATION', '/Volumes/DATA/workspace/shopping/catalog/');
+define('DIR_SYSTEM', '/Volumes/DATA/workspace/shopping/system/');
+define('DIR_IMAGE', '/Volumes/DATA/workspace/shopping/image/');
+define('DIR_LANGUAGE', '/Volumes/DATA/workspace/shopping/catalog/language/');
+define('DIR_TEMPLATE', '/Volumes/DATA/workspace/shopping/catalog/view/theme/');
+define('DIR_CONFIG', '/Volumes/DATA/workspace/shopping/system/config/');
+define('DIR_CACHE', '/Volumes/DATA/workspace/shopping/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Volumes/DATA/workspace/shopping/system/storage/download/');
+define('DIR_LOGS', '/Volumes/DATA/workspace/shopping/system/storage/logs/');
+define('DIR_MODIFICATION', '/Volumes/DATA/workspace/shopping/system/storage/modification/');
+define('DIR_UPLOAD', '/Volumes/DATA/workspace/shopping/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shopping');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
